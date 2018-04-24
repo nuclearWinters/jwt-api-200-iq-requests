c4880dc02099b7db841745b7afbc9ed886b6441b
nuclearWinters <armandonarcizoruedaperez@gmail.com>
2018-04-23 19:56:38 -0500

    added django-heroku

--- apiloginexternalrequests/settings.py
+++ apiloginexternalrequests/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import datetime
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +27,7 @@ SECRET_KEY = 'py*2&q&s*4_^y-h)+#lqnr-!_4ujsy^!=ze3x6!zgi7fznxjd7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['loginexternalrequests200iqapi.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -121,12 +122,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
